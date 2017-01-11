fc7318162891bfe1962cb1b462ccd0adead65b27
Mandeep Mehra <mandeep.mehra.2012@gmail.com>
2017-01-11 13:20:52 -0600

    Check Query with no polling

---
+++ src/main/resources/jira/CheckQueryNoPoll.py
@@ -0,0 +1,34 @@
+#
+# THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+# FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS.
+#
+
+import time
+from jira.JiraServerExt import JiraServerExt
+
+jira = JiraServerExt(jiraServer, username, password)
+
+tickets = jira.checkQuery(query)
+
+issues = {}
+count = 0
+matchedStatus = 0
+tickets = jira.checkQuery(query)
+
+for key, value in tickets.items():
+    if value[1] == expectedStatus:
+         matchedStatus = matchedStatus + 1
+         issues[key] = "%s - (%s)" % value
+    # end if
+# End for
+
+
+if matchedStatus > 0:
+   print "#### Issues found"
+   for key, value in issues.items():
+       print u"* {0} - {1}".format(jira._link(key), value)
+   sys.exit(1)
+else:
+     print "#### No issues found"
+# end if
